872b114d35ebdf902367bca0e3349e15c3774a8c
Fouz <Fouz6@outlook.com>
2023-05-16 13:54:28 +0500

    final

--- Tokenizer.py
+++ Tokenizer.py
@@ -20,11 +20,23 @@ class Tokenizer:
         else:
             self.currentChar = None
 
+    def retreat(self):
+        self.current -= 1
+        if (self.current > 0):
+            self.currentChar = self.source[self.current]
+            try:
+                self.nextChar = self.source[self.current + 1]
+            except:
+                self.nextChar = None
+        else:
+            self.currentChar = None
+
+
     def tokenize(self):
         while (self.currentChar is not None):
 
 
-            if (self.currentChar in '{(,)}='):
+            if (self.currentChar in '{(,)}=;'):
                 self.tokens.append(self.currentChar)
                 self.advance()
                 continue
@@ -32,6 +44,10 @@ class Tokenizer:
             if (self.currentChar == '-' and self.nextChar == '>'):
                 self.tokens.append(self.currentChar+self.nextChar)
                 self.advance()
+                self.advance()
+                continue
+            elif(self.currentChar == '-' and self.nextChar != '>'):
+                self.tokens.append(self.currentChar)
                 self.advance()
                 continue
 
@@ -75,10 +91,17 @@ class Tokenizer:
                 self.advance()
                 continue
 
+            if(self.currentChar == '\n'):
+                self.advance()
+                continue
+
             if(self.currentChar != ' '):
                 randomString = self.currentChar
                 self.advance()
                 while(self.currentChar != ' '):
+                    if(self.currentChar in "{(,)}=;-@"):
+                        self.retreat()
+                        break
                     randomString += self.currentChar
                     self.advance()
                 self.tokens.append(randomString)
@@ -88,7 +111,8 @@ class Tokenizer:
 
             self.advance()
         print(self.tokens)
+        return self.tokens
 
 
-Tokenizer = Tokenizer('@x1123k -> num = ("hello")//"HELLO"\n')
-Tokenizer.tokenize()
+# Tokenizer = Tokenizer('@x1 -> num = 1;\nloop(@x2 -> num = 50,0,-1){@x3 -> num = 1;}')
+# Tokenizer.tokenize()
